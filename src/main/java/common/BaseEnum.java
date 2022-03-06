1057656106f103098e4f66dee8e25cc2feff6dfa
newkayak12 <newkayak12@gmail.com>
2022-03-07 08:56:03 +0900

    common

---
+++ src/main/java/common/BaseEnum.java
@@ -0,0 +1,5 @@
+package common;
+
+public interface BaseEnum {
+    Integer getCode();
+}
---
+++ src/main/java/common/Reservation.java
@@ -0,0 +1,13 @@
+package common;
+
+
+public enum Reservation {
+    RESERVATION(1);
+
+
+
+    int code;
+    Reservation(int code){
+        this.code=code;
+    }
+}
